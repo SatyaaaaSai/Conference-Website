8fd48c4244f3acb10a8450b78787aa3ca26e7690
Satya Sai <satya07thota@gmail.com>
2023-12-07 21:17:54 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -4,42 +4,26 @@ import HomePage from "./components/HomePage";
 import ProductPage from "./components/ProductPage";
 import AboutPage from "./components/AboutPage";
 import ContactPage from "./components/ContactPage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
-      <HomePage />
-      <ProductPage />
-      <AboutPage />
-      <ContactPage />
+      <Routes>
+      <Route exact path="/" Component={HomePage} />
+      <Route path="/products" Component={ProductPage} />
+      <Route path="/about" Component={AboutPage} />
+      <Route path="/contact" Component={ContactPage} />
+    </Routes>
     </div>
   );
 };
 
-const AppRouter = createBrowserRouter([
-  {
-    path: "/",
-    Element: <AppLayout />,
-  },
-  {
-    path: "/products",
-    Element: <ProductPage />,
-  },
-  {
-    path: "/about",
-    Element: <AboutPage />,
-  },
-  {
-    path: "/contact",
-    Element: <ContactPage />,
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<AppLayout/>)
-
-root.render(<RouterProvider router={AppLayout}/>);
+root.render(
+  <Router>
+    <AppLayout />
+  </Router>
+);
