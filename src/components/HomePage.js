34a2cc8eea826ac897760110c75a8f30e92eebcf
Satya Sai <satya07thota@gmail.com>
2023-12-07 23:16:39 +0530

    half  header

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,29 @@
+import { logo } from "../utils/constants";
+import { Link } from "react-router-dom";
 const HomePage=()=>{
     return(
         <div>
-            <h1>I am Home Page</h1>
+           <div className="flex justify-between">
+            <div className="flex justify-center items-center">
+                <img className="p-2 w-8 flex items-center" src={logo} alt="logo"/> 
+            </div>
+            <div>
+                <ul className="flex justify-center">
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/">Home</a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/products">Products</a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/about">About</a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex justify-center">
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/">Log  </a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/products">Products</a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/about">About</a></li>
+                    <li className="m-3 p-3 hover:cursor-pointer"><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
+           </div>
         </div>
     )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const logo =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIc9fnyfGA4nyhSWB6QS_57XWtu7M8OTizE-6VdVwGXV_THaslus9Kpbbg5YDO2AtW3h8&usqp=CAU0";
